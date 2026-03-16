586138cca22f01d294ace026ed73bbf1c1354a5d
Darsh kashyap <dkdarshkashyap07@gmail.com>
2026-03-16 21:16:12 +0530

    created footer component

---
+++ frontend/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>&copy; {new Date().getFullYear()} Coursera. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
